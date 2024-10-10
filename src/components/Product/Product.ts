7f60b9d0af08d4ef6089ed0785f1d77ed4ca03ac
SilvanaCuartasS <silvanacuartassilva@gmail.com>
2024-10-09 22:10:01 -0500

    componenetes

--- src/components/Product/Product.ts
+++ src/components/Product/Product.ts
@@ -1,23 +1,22 @@
 export enum Attribute {
+    "buttonAddShoppingCart" = "buttonAddShoppingCart",
     "image" = "image",
-    "name" = "name",
-    "status" = "status",
-    "species" = "species",
-    "type" = "type",
-    "origin" = "origin",
-    "firstepisode" = "firstepisode",
+    "titleP" = "titleP",
+    "description" = "description",
+    "category" = "category",
+    "price" = "price",
+    "rating" = "rating",
 }
 
 class Character extends HTMLElement {
 
+    buttonAddShoppingCart?: number;
     image?: string;
-    name?: string;
-    status?: string;
-    species?: string;
-    type?: string;
-    origin?: string;
-    area?: string;
-    firstepisode?: string;
+    titleP?: string;
+    description?: string;
+    category?: string;
+    price?: number;
+    rating?: number;
 
     static get observedAttributes(){
         return Object.keys(Attribute); //  return ["image","uid","age".....]
@@ -30,8 +29,22 @@ class Character extends HTMLElement {
 
     attributeChangedCallback(propName:Attribute,oldValue: string | undefined,newValue: string | undefined){
         switch(propName){
+
+            case Attribute.buttonAddShoppingCart:
+                this.price = newValue ? Number(newValue) : undefined;
+                break;
+
+            case Attribute.price:
+                this.price = newValue ? Number(newValue) : undefined;
+                break;
+            
+            case Attribute.rating:
+                this.price = newValue ? Number(newValue) : undefined;
+                break;
             
             default: 
+
+
             this[propName] = newValue;
             break;
         }
@@ -46,26 +59,30 @@ class Character extends HTMLElement {
     render() {
         if(this.shadowRoot){
             this.shadowRoot.innerHTML = `
-
-            <link rel="stylesheet" href="../src/components/Character/Character.css">
-          
-            <div id="card">
-            <div>
-            <img src="${this.image || 'No image'}" alt="imagen de: ${this.name}">
+        <link rel="stylesheet" href="../src/components/product/product.css">
+        <div class="product">
+            <div class="image">
+                <img src="${this.image}" || 'No Image'}">
             </div>
-
-            <div id="txt">
-            <h1>${this.name || 'No name'}</h1>
-            <p>Status: ${this.status || 'No status'}</p>
-            <p>Specie: ${this.species || 'No specie'}</p>
-            <p>Type: ${this.type || 'No type'}</p>
-            <p>Origin: ${this.origin || 'No origin'}</p>
-            <p>First episode: ${this.firstepisode || 'No episode yet'}</p>
+            <div class="information">
+                <div class="details">
+                    <h3>${this.titleP || 'No Title'}</h3>
+                    <div class="description">
+                        <p >${this.description || 'No Description'}</p>
+                    </div>
+                    <p class="category">Category: ${this.category || 'No Category'}</p>
+                    <p class="rating">Rating: ${this.rating || 'No  Rating'}</p>
+                    <p class="price">$${this.price || 'No Price'}</p>
+                    
+                    <button>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" viewBox="0 0 24 24"><path fill="currentColor" d="M11 9h2V6h3V4h-3V1h-2v3H8v2h3M7 18c-1.1 0-2 .9-2 2s.9 2 2 2s2-.9 2-2s-.9-2-2-2m10 0c-1.1 0-2 .9-2 2s.9 2 2 2s2-.9 2-2s-.9-2-2-2m-9.8-3.2v-.1l.9-1.7h7.4c.7 0 1.4-.4 1.7-1l3.9-7l-1.7-1l-3.9 7h-7L4.3 2H1v2h2l3.6 7.6L5.2 14c-.1.3-.2.6-.2 1c0 1.1.9 2 2 2h12v-2H7.4c-.1 0-.2-.1-.2-.2"/></svg>
+                    </button>
+                </div>
             </div>
-
-            </div>
+        </div>       
             `
         }
+
     }
 }
 
